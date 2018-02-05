95b426fc771d4eb4069d5931e1138a7348083e74
tianchu-shu <35506630+tianchu-shu@users.noreply.github.com>
2018-02-04 20:46:32 -0600

    Add files via upload

--- hospital_info_datafilter.py
+++ hospital_info_datafilter.py
@@ -1,8 +1,14 @@
+# Tianchu Shu
 import pandas as pd
-import numpy as np
 import csv
 
 df = pd.read_csv("Hospital_General_Information.csv")
 
-df_ER = df[df["Emergency Services"] == True]
+#remove the white space in the column name with underscore
+df.columns = df.columns.str.replace('\s+', '_')
+
 #filter the hospitals with emergency room
+df_ER = df[df["Emergency_Services"] == True]
+
+
+df_ER.to_csv("HGI.csv", encoding='utf-8', index=False)
