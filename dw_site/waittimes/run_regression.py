b069666afeefb4dfe0fb9c562aaed5e6d8e20141
tianchu-shu <35506630+tianchu-shu@users.noreply.github.com>
2018-03-07 12:08:47 -0600

    Update run_regression.py

--- dw_site/waittimes/run_regression.py
+++ dw_site/waittimes/run_regression.py
@@ -4,6 +4,9 @@ import pickle
 MEDIAN_AVGWAIT = 42
 
 def find_model():
+    '''
+    Load our dataset into pandas dataframe and load the ols model we saved
+    '''
     df = regression.clean_data("waittimes/nhamcs_all_data.csv")
     # load the model from disk
     model = pickle.load(open("waittimes/finalized_model.sav", 'rb'))
@@ -11,15 +14,22 @@ def find_model():
 
 
 def run_regression(user_pain, hosp_qs, model, df):
+    '''
+    Getting the real time, painscale and average wait time of the nearest
+    five hospitals into our pandas dataframe to run the regression
+    '''
     x = regression.user_time(df)
     x["PAINSCALE"] = user_pain
     for i in range(len(hosp_qs)):
+        # if the hospital doesnt have a average waittime using natioanl median instead
         if hosp_qs[i].score == -1:
             x.loc[i, "AVGWAIT"] = MEDIAN_AVGWAIT
         x.loc[i, "AVGWAIT"] = hosp_qs[i].score
         if hosp_qs[i].msa == "Metropolitan Statistical Area":
             x.loc[i, "MSA"] = 1
         x = x[:len(hosp_qs)]
+        if len(hosp_qs) == 1:
+            x = x.reshape(1, -1)             
 
     predictions = model.predict(x)
     for i, pred in enumerate(predictions):
